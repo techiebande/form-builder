c230f266a166c8337a7a3df5e2b28be97238b955
samuelochaba <nerdysam5826@gmail.com>
2023-12-10 11:03:42 +0100

    switch to postgress

--- prisma/migrations/20231210094928_init_db/migration.sql
+++ prisma/migrations/20231210094928_init_db/migration.sql
@@ -1,24 +1,27 @@
 -- CreateTable
 CREATE TABLE "Form" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "userId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "published" BOOLEAN NOT NULL DEFAULT false,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL DEFAULT '',
     "content" TEXT NOT NULL DEFAULT '[]',
     "visits" INTEGER NOT NULL DEFAULT 0,
     "submissions" INTEGER NOT NULL DEFAULT 0,
-    "shareURL" TEXT NOT NULL
+    "shareURL" TEXT NOT NULL,
+
+    CONSTRAINT "Form_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "FormSubmissions" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "formId" INTEGER NOT NULL,
     "content" TEXT NOT NULL,
-    CONSTRAINT "FormSubmissions_formId_fkey" FOREIGN KEY ("formId") REFERENCES "Form" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "FormSubmissions_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -26,3 +29,6 @@ CREATE UNIQUE INDEX "Form_shareURL_key" ON "Form"("shareURL");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Form_name_userId_key" ON "Form"("name", "userId");
+
+-- AddForeignKey
+ALTER TABLE "FormSubmissions" ADD CONSTRAINT "FormSubmissions_formId_fkey" FOREIGN KEY ("formId") REFERENCES "Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
